97728e4ee0a18fb66e3c600386393b82476ccefc
Michael Klein <michaelklein1997@gmail.com>
2026-04-25 19:54:30 -0500

    setup initial project structure

---
+++ services/cart/src/main/java/com/michaelklein/ecommerce/CartService.java
@@ -0,0 +1,13 @@
+package com.michaelklein.ecommerce.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartService.class, args);
+    }
+
+}
---
+++ services/item/src/main/java/com/michaelklein/ecommerce/item/ItemService.java
@@ -0,0 +1,13 @@
+package com.michaelklein.ecommerce.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemService.class, args);
+    }
+
+}
